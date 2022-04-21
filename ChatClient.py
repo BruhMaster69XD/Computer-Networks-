8de3d9751288aa00938347c3e10e91dcdc5491de
Philipp Habti <97538075+BruhMaster69XD@users.noreply.github.com>
2022-04-21 12:31:32 +0200

    Update ChatClient.py

--- ChatClient.py
+++ ChatClient.py
@@ -1,31 +1,43 @@
 import socket
 import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_port = ("143.47.184.219", 5378)
-sock.connect(host_port)
 
-def readMessages():
+
+
+def readMessages(sock):
     while True:
         
-        rData = sock.recv(4096)
+        while True:
+            accumulator = sock.recv(1)
+            checker = accumulator[:-2]
+            checker += accumulator[:-1]
+            rData += accumulator
+
+            if checker == "\n":
+                break
+
         rData2 = rData
         rData2 = rData2[0:7]
         rData = rData.decode("utf-8")
         rData2 = rData2.decode("utf-8")
         if rData2 == "WHO-OK":
             print(rData)
-        elif rData2 == "SEND-Ok":
+        elif rData2 == "SEND-OK":
             print("your message was sent succesfully")
         elif rData2 == "UNKNOWN":
             print("user is not logged in")
+        elif rData2 == "BAD-RQST-HDR":
+            print("your header was incorrect")
+        elif rData2 == "BAD-RQST-BODY":
+            print("something went wrong with your body")
         else:
             rData = rData[8:]
             print(rData)
 
-t = threading.Thread(target=readMessages)
 
-def OurProgram() :
+
+
+def OurProgram(sock) :
 
 
     message = "HELLO-FROM "
@@ -34,13 +46,21 @@ def OurProgram() :
     string_bytes = message.encode("utf-8")
     sock.sendall(string_bytes)
 
-    data = sock.recv(4096)
+    while True:
+            accumulator = sock.recv(1)
+            checker = accumulator[:-2]
+            checker += accumulator[:-1]
+            data += accumulator
+
+            if checker == "\n":
+                break
 
     decoded = data.decode()
     decoded = decoded [:-1]
 
             
     while decoded == "IN-USE":
+        
         print("the username was already taken")
         message = "HELLO-FROM "
         message += input("please enter a username: ")
@@ -52,6 +72,13 @@ def OurProgram() :
 
         decoded = data.decode()
         decoded = decoded [:-1]
+        sock.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host_port = ("143.47.184.219", 5378)
+        sock.connect(host_port)
+
+    t = threading.Thread(target=readMessages, args = (sock,))
+    t.daemon = True
       
         
     t.start()
@@ -72,7 +99,8 @@ def OurProgram() :
                 
             elif MainInput == "!QUIT":
                 print("You are now being logged off")
-                OurProgram()
+                break
+                
                 
 
             else:
@@ -89,4 +117,8 @@ def OurProgram() :
            
 
 
-OurProgram()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_port = ("143.47.184.219", 5378)
+sock.connect(host_port)
+
+OurProgram(sock)
